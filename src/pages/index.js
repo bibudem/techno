837805821926711e2dafd3c1026c98968f3938e3
emirgc <emir.chouchane@umontreal.ca>
2025-04-03 01:30:59 -0400

    changement titre

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Les bibliothèques UdeM - ${siteConfig.title}`}
+      description="Un site pour découvrir les ressources et services offerts par les bibliothèques de l'Université de Montréal.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
