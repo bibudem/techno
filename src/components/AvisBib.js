8fbd1991044b6fc11628344872969ae801a8c498
emirgc <emir.chouchane@umontreal.ca>
2025-06-12 16:28:04 -0400

    Mise à jour de la version du script bib-avis à la version 1

--- src/components/AvisBib.js
+++ src/components/AvisBib.js
@@ -4,7 +4,7 @@ export default function AvisBib() {
   useEffect(() => {
     // Charger dynamiquement le script
     const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-avis.min.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/bibudem/ui@1/dist/bib-avis.min.js';
     script.type = 'module';
     script.async = true;
     document.body.appendChild(script);
