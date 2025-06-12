7ae82d0cae5264e474c801f4812bda20f3da6882
emirgc <emir.chouchane@umontreal.ca>
2025-06-12 10:07:56 -0400

    Ajout du composant AvisBib

---
+++ src/components/AvisBib.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+export default function AvisBib() {
+  useEffect(() => {
+    // Charger dynamiquement le script
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/bibudem/ui@0.23.0/dist/bib-avis.min.js';
+    script.type = 'module';
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <bib-avis
+      service="https://avis.bib.umontreal.ca/api/avis"
+      bouton-fermer
+      fluide
+    ></bib-avis>
+  );
+}
--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -2,11 +2,13 @@
 import Layout from '@theme-original/Layout';
 import LienExterne from '@site/src/components/LienExterne';
 import LibChatWidget from '@site/src/components/LibChatWidget';
+import AvisBib from '@site/src/components/AvisBib';
 
 
 export default function LayoutWrapper(props) {
   return (
     <>
+      <AvisBib />
       <Layout {...props} />
       <LienExterne />
 <LibChatWidget hash="2cdfcaf0f2e776291e8bfc9296e8b7573083d9b6ad33c930fa46024633447cce" />    </>
