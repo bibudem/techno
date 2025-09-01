162fe2f1afe42af847486aa5d57dfa35f675364e
emirgc <emir.chouchane@umontreal.ca>
2025-09-01 00:23:57 -0400

    Ajout de l'icone House dans le breadcrumbs

--- src/theme/DocBreadcrumbs/Items/Home/index.js
+++ src/theme/DocBreadcrumbs/Items/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
+import { House } from '@phosphor-icons/react';
 
 export default function HomeBreadcrumbItem() {
   return (
@@ -7,14 +8,17 @@ export default function HomeBreadcrumbItem() {
       <li className="breadcrumbs__item">
         <Link
           className="breadcrumbs__link"
-          href="https://bib.umontreal.ca">
+          href="https://bib.umontreal.ca"
+        >
+          <House size={16} style={{ verticalAlign: 'text-bottom', marginRight: '0.25rem' }} />
           Les bibliothèques UdeM
         </Link>
       </li>
       <li className="breadcrumbs__item">
         <Link
           className="breadcrumbs__link"
-          href="/">
+          href="/"
+        >
           studio·bib
         </Link>
       </li>
